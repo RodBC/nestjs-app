86c566e037a4a7408f5595230d0b4530bc85235d
RodBC <rbc6@cin.ufpe.br>
2022-07-20 11:36:33 -0300

    hotfix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppDataSource } from './data-source';
 
 async function bootstrap() {
-  AppDataSource.initialize().then(async () => {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
-  });
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tasks app')
+    .setDescription('The tasks app')
+    .setVersion('1.0')
+    .build();
+
+  await AppDataSource.initialize()
+    .then(() => console.log('bd up'))
+    .catch((error) => console.log(error));
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  await app.listen(3000);
 }
 
 bootstrap();
